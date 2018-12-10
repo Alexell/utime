5b66603a0babd8dd03a3a37390074df4e0adc540
Alexell <alexell@alexell.ru>
2018-12-10 14:20:19 +0300

    Replaced UniqueID() to SteamID()
    For ease of viewing sv.db

--- lua/autorun/sv_utime.lua
+++ lua/autorun/sv_utime.lua
@@ -5,26 +5,25 @@ if not SERVER then return end
 utime_welcome = CreateConVar( "utime_welcome", "1", FCVAR_ARCHIVE )
 
 if not sql.TableExists( "utime" ) then
-	sql.Query( "CREATE TABLE IF NOT EXISTS utime ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, player INTEGER NOT NULL, totaltime INTEGER NOT NULL, lastvisit INTEGER NOT NULL );" )
-	sql.Query( "CREATE INDEX IDX_UTIME_PLAYER ON utime ( player DESC );" )
+	sql.Query( "CREATE TABLE IF NOT EXISTS utime ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, player TEXT NOT NULL, totaltime INTEGER NOT NULL, lastvisit INTEGER NOT NULL );" )
 end
 
 function onJoin( ply )
-	local uid = ply:UniqueID()
-	local row = sql.QueryRow( "SELECT totaltime, lastvisit FROM utime WHERE player = " .. uid .. ";" )
+	local sid = ply:SteamID()
+	local row = sql.QueryRow( "SELECT totaltime, lastvisit FROM utime WHERE player = '" .. sid .. "';" )
 	local time = 0 
 
 	if row then
 		if utime_welcome:GetBool() then
 			ULib.tsay( ply, "[UTime]Welcome back " .. ply:Nick() .. ", you last played on this server " .. os.date( "%c", row.lastvisit ) )
 		end
-		sql.Query( "UPDATE utime SET lastvisit = " .. os.time() .. " WHERE player = " .. uid .. ";" )
+		sql.Query( "UPDATE utime SET lastvisit = " .. os.time() .. " WHERE player = '" .. sid .. "';" )
 		time = row.totaltime
 	else
 		if utime_welcome:GetBool() then
 			ULib.tsay( ply, "[UTime]Welcome to our server " .. ply:Nick() .. "!" )
 		end
-		sql.Query( "INSERT into utime ( player, totaltime, lastvisit ) VALUES ( " .. uid .. ", 0, " .. os.time() .. " );" )
+		sql.Query( "INSERT into utime ( player, totaltime, lastvisit ) VALUES ( '" .. sid .. "', 0, " .. os.time() .. " );" )
 	end
 	ply:SetUTime( time )
 	ply:SetUTimeStart( CurTime() )
@@ -32,7 +31,7 @@ end
 hook.Add( "PlayerInitialSpawn", "UTimeInitialSpawn", onJoin )
 
 function updatePlayer( ply )
-	sql.Query( "UPDATE utime SET totaltime = " .. math.floor( ply:GetUTimeTotalTime() ) .. " WHERE player = " .. ply:UniqueID() .. ";" )
+	sql.Query( "UPDATE utime SET totaltime = " .. math.floor( ply:GetUTimeTotalTime() ) .. " WHERE player = '" .. ply:SteamID() .. "';" )
 end
 hook.Add( "PlayerDisconnected", "UTimeDisconnect", updatePlayer )
 
